818826b1e1c47d397c616314402060d4e4b17ff6
KoolVIVIT <ZubahDuhba@protonmail.com>
2024-04-17 19:59:41 +0200

    Clean code

--- YuukoAlert.py
+++ YuukoAlert.py
@@ -2,28 +2,41 @@ import pygame
 import tkinter as tk
 from PIL import Image,ImageTk
 
-pygame.init()
-root = tk.Tk()
-root.resizable(False,False)
-root.geometry("250x150")
-root.eval('tk::PlaceWindow . center')
-root.iconphoto(False, tk.PhotoImage(file='files/icon.png'))
-root.title("Yuuko Alert")
+def show_alert():
+	pygame.init()
+	root = tk.Tk()
+	root.resizable(False,False)
+	root.geometry("250x150")
+	root.eval('tk::PlaceWindow . center')
+	root.iconphoto(False, tk.PhotoImage(file='files/icon.png'))
+	root.title("Yuuko Alert")
 
-canvas= tk.Canvas(root, width= 250, height= 80)
-canvas.pack(padx=5, pady=5)
+	canvas= tk.Canvas(root, width= 250, height= 80)
+	canvas.pack(padx=5, pady=5)
 
-img= (Image.open("files/face.png"))
-resized_image= img.resize((80,72), Image.LANCZOS)
-new_image= ImageTk.PhotoImage(resized_image)
-canvas.create_image(10,10, anchor="nw", image=new_image)
-canvas.create_text(170, 50, text="Selamat Pagi !", fill="black", font=('Segoe UI', '11'))
+	with Image.open("files/face.png") as img:
+		resized_image= img.resize((80,72), Image.LANCZOS)
+		new_image= ImageTk.PhotoImage(resized_image)
+		canvas.create_image(10,10, anchor="nw", image=new_image)
 
-close = tk.Button(root, text="        OK        ", font=("Segoe UI", "10"), command=root.destroy)
-close.pack(padx=5, pady=5)
+	canvas.create_text(170, 50, text="Selamat Pagi !", fill="black", font=('Segoe UI', '11'))
 
-my_sound = pygame.mixer.Sound('files/sound.MP3')
-my_sound.play()
-my_sound.set_volume(0.8)
+	close = tk.Button(root, text="        OK        ", font=("Segoe UI", "10"), command=root.destroy)
+	close.pack(padx=5, pady=5)
 
-root.mainloop()
+	pygame.mixer.init()
+	my_sound = pygame.mixer.Sound('files/sound.MP3')
+	my_sound.play()
+	my_sound.set_volume(0.8)
+
+	root.mainloop()
+
+if __name__ == "__main__":
+
+	try:
+		import setproctitle
+		setproctitle.setproctitle('Yuuko Alert')
+	except ImportError:
+		pass
+
+	show_alert()
